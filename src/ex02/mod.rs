8f77bc0c03ecbe4b629e0ab9f22bb071006c535f
dlu <odon5ht@gmail.com>
2023-11-09 12:56:25 +0100

    Add tests for gray code

--- src/ex02/mod.rs
+++ src/ex02/mod.rs
@@ -1,3 +1,18 @@
 pub fn gray_code(n: u32) -> u32 {
 	n ^ (n >> 1)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gray_code_general() {
+        assert_eq!(gray_code(10), 15);
+		assert_eq!(gray_code(16), 0b11000);
+		assert_eq!(gray_code(17), 0b11001);
+		assert_eq!(gray_code(18), 0b11011);
+		assert_eq!(gray_code(19), 0b11010);
+		assert_eq!(gray_code(20), 0b11110);
+    }
+}
